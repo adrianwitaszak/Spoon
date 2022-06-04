3a45d4e3df4e3aa5a4d22a7ec58459bdcbd8d0fb
Adrian Witaszak <adrian.witaszak@cognetivity.com>
2022-06-04 11:08:32 +0100

    reformat

--- shared/src/commonMain/kotlin/com/adwi/spoon/model/ExtraIngredient.kt
+++ shared/src/commonMain/kotlin/com/adwi/spoon/model/ExtraIngredient.kt
@@ -5,16 +5,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ExtendedIngredient(
-    @SerialName("amount")
-    val amount: Double = 0.0,
-    @SerialName("consistency")
-    val consistency: String = "",
-    @SerialName("image")
-    val image: String = "",
-    @SerialName("name")
-    val name: String = "",
-    @SerialName("original")
-    val original: String = "",
-    @SerialName("unit")
-    val unit: String = ""
+    @SerialName("name") val name: String = "",
+    @SerialName("image") val image: String = "",
+    @SerialName("amount") val amount: Double = 0.0,
+    @SerialName("unit") val unit: String = "",
+    @SerialName("consistency") val consistency: String = "",
+    @SerialName("original") val original: String = "",
 )
--- shared/src/commonMain/kotlin/com/adwi/spoon/model/FoodRecipe.kt
+++ shared/src/commonMain/kotlin/com/adwi/spoon/model/FoodRecipe.kt
@@ -5,34 +5,19 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class FoodRecipe(
-    @SerialName("aggregateLikes")
-    val aggregateLikes: Int = 0,
-    @SerialName("cheap")
-    val cheap: Boolean = false,
-    @SerialName("dairyFree")
-    val dairyFree: Boolean = false,
-    @SerialName("extendedIngredients")
-    val extendedIngredients: List<ExtendedIngredient> = emptyList(),
-    @SerialName("glutenFree")
-    val glutenFree: Boolean = false,
-    @SerialName("id")
-    val id: Int = 0,
-    @SerialName("image")
-    val image: String = "",
-    @SerialName("readyInMinutes")
-    val readyInMinutes: Int = 0,
-    @SerialName("sourceName")
-    val sourceName: String = "",
-    @SerialName("sourceUrl")
-    val sourceUrl: String = "",
-    @SerialName("summary")
-    val summary: String = "",
-    @SerialName("title")
-    val title: String = "",
-    @SerialName("vegan")
-    val vegan: Boolean = false,
-    @SerialName("vegetarian")
-    val vegetarian: Boolean = false,
-    @SerialName("veryHealthy")
-    val veryHealthy: Boolean = false,
+    @SerialName("id") val id: Int = 0,
+    @SerialName("title") val title: String = "",
+    @SerialName("summary") val summary: String = "",
+    @SerialName("sourceName") val sourceName: String = "",
+    @SerialName("sourceUrl") val sourceUrl: String = "",
+    @SerialName("image") val image: String = "",
+    @SerialName("readyInMinutes") val readyInMinutes: Int = 0,
+    @SerialName("vegan") val vegan: Boolean = false,
+    @SerialName("vegetarian") val vegetarian: Boolean = false,
+    @SerialName("veryHealthy") val veryHealthy: Boolean = false,
+    @SerialName("aggregateLikes") val aggregateLikes: Int = 0,
+    @SerialName("cheap") val cheap: Boolean = false,
+    @SerialName("dairyFree") val dairyFree: Boolean = false,
+    @SerialName("glutenFree") val glutenFree: Boolean = false,
+    @SerialName("extendedIngredients") val extendedIngredients: List<ExtendedIngredient> = emptyList(),
 )
